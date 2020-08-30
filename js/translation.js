c8fc6b89044e77d69d805021987a5a7b2c869a15
james60708 <b05505004@csie.ntu.edu.tw>
2020-08-30 11:58:13 +0800

    add register

--- js/translation.js
+++ js/translation.js
@@ -30,9 +30,9 @@ else{ // cookie === "undefined"
 var agenda = document.getElementById("agenda_pic");
 if(agenda != null){
     if(current === 0)
-        document.getElementById("agenda_pic").src="./img/agenda-en.jpg";
+        document.getElementById("agenda_pic").src="./img/agenda-en.png";
     else
-        document.getElementById("agenda_pic").src="./img/agenda-zh.jpg";
+        document.getElementById("agenda_pic").src="./img/agenda-zh.png";
 }
 
 
